05f48cff84ff995ccebcb2a4800be84cb9851faa
Yisen Jin <jeason2020@gmail.com>
2024-08-10 20:13:48 -0400

    add feature of selecting subject ids to display

--- webapp/R/results-tgi.R
+++ webapp/R/results-tgi.R
@@ -1,4 +1,4 @@
-get_tgi_results <- function(df, input, output) {
+get_tgi_results <- function(df, input, output, selected = NULL) {
     output$tgi_data_preview <- renderTable({
         head(df, 10)
     })
@@ -16,7 +16,9 @@ get_tgi_results <- function(df, input, output) {
     })
 
     output$tgi_fit_plot <- renderPlot({
-        selected <- sample(1:length(unique(df$SUBJID)), 5)
+        if (is.null(selected)) {
+            selected <- sample(1:length(unique(df$SUBJID)), 5)
+        }
 
         # 提取所选数据点的Y值
         selected_Y_values <- unlist(lapply(selected, function(id) subset(df, SUBJID == id)$Y))
@@ -24,21 +26,37 @@ get_tgi_results <- function(df, input, output) {
         # 计算Y轴范围
         y_range <- range(selected_Y_values)
 
+        col_vals <- 1:length(selected)
+
+
+        par(mar = c(6, 4, 4, 2) + 0.1, xpd = TRUE) 
+
         ii <- 1
-        plot(subset(df, SUBJID == selected[ii])$TIME, subset(df, SUBJID == selected[ii])$Y, type = "p", xlab = "Time", ylab = "Y", pch = 15, col = ii, ylim = y_range,
+        plot(subset(df, SUBJID == selected[ii])$TIME, subset(df, SUBJID == selected[ii])$Y, type = "p", xlab = "Time", ylab = "Y", pch = 16, col = ii, ylim = y_range,
         main = "Selected subjects with data points and fitted lines")
-        lines(subset(df, SUBJID == selected[ii])$TIME, predict(fit, newdata = subset(df, SUBJID == selected[ii])), col = ii)
+        lines(subset(df, SUBJID == selected[ii])$TIME, predict(fit, newdata = subset(df, SUBJID == selected[ii])), col = col_vals[ii])
 
 
         for (ii in 2:length(selected)) {
-            points(subset(df, SUBJID == selected[ii])$TIME, subset(df, SUBJID == selected[ii])$Y, type = "p", pch = 14 + ii, col = ii)
-            lines(subset(df, SUBJID == selected[ii])$TIME, predict(fit, newdata = subset(df, SUBJID == selected[ii])), col = ii)
+            points(subset(df, SUBJID == selected[ii])$TIME, subset(df, SUBJID == selected[ii])$Y, type = "p", pch = 16, col = col_vals[ii])
+            lines(subset(df, SUBJID == selected[ii])$TIME, predict(fit, newdata = subset(df, SUBJID == selected[ii])), col = col_vals[ii])
         }
-        legend("topleft", legend = paste("SUBJID", selected), pch = 15:19, col = 1:5)
+
+        if (length(selected) < 10) {
+            legend("topleft", legend = paste("SUBJID", selected), pch = 16, col = col_vals)
+        }
+        
     })
 
     output$tgi_resFitted <- renderPlot({
         plot(fit, main = "Standardized residuals v.s. Fitted values", col = "black", pch = 16)
     })
+
+    output$tgi_qqplot <- renderPlot({
+        qqnorm(fit$residuals, main = "Normal Q-Q plot", pch = 16)
+        qqline(fit$residuals)
+    })
+
+
     
 }
